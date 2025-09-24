998d10f86d59ea5c86c28ff180a5a03b3a33c4e3
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-09-24 10:23:28 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 57ms, Memory: 16MB, Status: Passed - Codetree

--- 250924/중복되지 않는 정수 중 최대/max-of-unique-number.py
+++ 250924/중복되지 않는 정수 중 최대/max-of-unique-number.py
@@ -1,13 +1,16 @@
-n = int(input())
-nums = list(map(int, input().split()))
+# 입력 받기
+N = int(input())
+arr = list(map(int, input().split()))
 
-cnt = [0] * (max(nums)+1)
-for i in nums :
-    cnt[i] += 1
+# 각 숫자의 등장 횟수 세기
+from collections import Counter
+count = Counter(arr)
 
-max_val = -1
-for i in range(len(cnt)) :
-    if cnt[i] == 1 :
-        max_val = i
+# 등장 횟수가 1인 숫자들만 필터링
+unique_numbers = [num for num, cnt in count.items() if cnt == 1]
 
-print(max_val)
+# 결과 출력
+if unique_numbers:
+    print(max(unique_numbers))
+else:
+    print(-1)
