4908d962e88148fcbaed0cbd31e0edd5997b468b
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-09-24 10:18:05 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 79ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250924/중복되지 않는 정수 중 최대/max-of-unique-number.py
@@ -0,0 +1,13 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+cnt = [0] * (max(nums)+1)
+for i in nums :
+    cnt[i] += 1
+
+max_val = -1
+for i in range(len(cnt)) :
+    if cnt[i] == 1 :
+        max_val = nums[i-1]
+
+print(max_val)
