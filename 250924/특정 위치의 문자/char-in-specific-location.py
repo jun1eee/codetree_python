443b8894a1687c6019453ceb1f729e7e18cbcfd5
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-09-24 09:37:13 +0900

    Title: 특정 위치의 문자, Time: 83ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250924/특정 위치의 문자/char-in-specific-location.py
@@ -0,0 +1,12 @@
+arr = [L,E,B,R,O,S]
+a = input()
+
+index = -1
+for i, char in enumerate(arr) :
+    if char == a :
+        index = i
+
+if index == -1 :
+    print("None")
+else :
+    print(index)
