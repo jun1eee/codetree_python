ddb61c46b9551811e65333bfe27112094460e27c
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-10-10 15:45:45 +0900

    Title: 함수를 이용한 369 게임, Time: 86ms, Memory: 18MB, Status: Passed - Codetree

--- 251010/함수를 이용한 369 게임/369-games-using-functions.py
+++ 251010/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,6 +1,6 @@
 import sys
 
-def has369(x: int) -> bool:
+def has369(x) :
     # x의 자릿수에 3, 6, 9가 하나라도 있으면 True
     while x:
         d = x % 10
@@ -9,9 +9,12 @@ def has369(x: int) -> bool:
         x //= 10
     return False
 
-def is_ok(n: int) -> bool:
+def is_ok(n) :
     return (n % 3 == 0) or has369(n)
 
-A, B = map(int, sys.stdin.readline().split())
-cnt = sum(1 for n in range(A, B + 1) if is_ok(n))
+A, B = map(int, input().split())
+cnt = 0
+for i in range(A, B + 1):
+    if is_ok(i):
+        cnt += 1
 print(cnt)
