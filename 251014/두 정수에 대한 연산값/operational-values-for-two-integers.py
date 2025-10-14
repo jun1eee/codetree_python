995b1eab837be15799a73387f846bd02c56d4f05
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-10-14 11:58:39 +0900

    Title: 두 정수에 대한 연산값, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251014/두 정수에 대한 연산값/operational-values-for-two-integers.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+
+def cal(a, b) :
+    if a > b :
+        a = a + 25
+        b = 2 * b
+    else :
+        a = 2 * a
+        b = b + 25
+    return a, b
+
+a, b = cal(a, b)
+print(a, b)
