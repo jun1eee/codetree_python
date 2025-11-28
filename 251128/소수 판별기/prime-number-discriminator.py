e50b6e92d59f7045c9f7702c00e51f2bdade97dd
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-11-28 14:34:09 +0900

    Title: 소수 판별기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251128/소수 판별기/prime-number-discriminator.py
+++ 251128/소수 판별기/prime-number-discriminator.py
@@ -1,8 +1,8 @@
 n = int(input())
 cnt = 0
-for i in range(2, round(n**0.5)+2):
+for i in range(2, round(n**0.5)+1):
     if (n%i)==0:
-        cnt ++ 1;
+        cnt += 1;
 if (cnt==0 or n==2):
     print("P")
 else:
