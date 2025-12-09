e7cdf2ba76a70e2a3c73bc0f9a71f67c648bab20
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-12-09 09:37:16 +0900

    Title: 떨어지는 1자 블록, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251209/떨어지는 1자 블록/falling-horizontal-block.py
+++ 251209/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,19 +1,23 @@
 n, m, k = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-for row in range(n) :
-    check = True
-    for j in range(k-1,k+m-1) :
-        if grid[row][j] != 0 :
-            check = False
+for row in range(n):
+    blocked = False
+    for j in range(k-1, k+m-1):
+        if grid[row][j] != 0:
+            blocked = True
             break
-    if not check : 
-        for j in range(k-1,k+m-1) :
-            grid[row-1][j] = 1
-        break
 
-for row in grid :
+    if blocked:
+        pos = row - 1
+        break
+    else:
+       pos = n - 1
+
+for j in range(k-1, k+m-1):
+    grid[pos][j] = 1
+
+for row in grid:
     for r in row :
         print(r, end=" ")
     print()
-    
