e5cf65d326fda94f2cd69ca64c1ea54a785e4fb8
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2025-12-15 16:05:51 +0900

    Title: 거꾸로 순열, Time: 182ms, Memory: 25MB, Status: Passed - Codetree

---
+++ 251215/거꾸로 순열/backward-permutation.py
@@ -0,0 +1,18 @@
+n = int(input())
+
+visited = [False] * (n+1)
+ans = []
+def back(num) :
+    if num == n+1 :
+        for a in ans :
+            print(a, end=" ")
+        print()
+    for i in range(n, 0, -1) :
+        if visited[i] :
+            continue
+        visited[i] = True
+        ans.append(i)
+        back(num+1)
+        ans.pop()
+        visited[i] = False
+back(1)
