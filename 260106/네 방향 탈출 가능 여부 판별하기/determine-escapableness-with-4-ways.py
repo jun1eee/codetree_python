8085933ea36ca4f1568a9ad37ac408338420535f
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2026-01-06 11:10:54 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 80ms, Memory: 18MB, Status: Passed - Codetree

--- 260106/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 260106/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -15,7 +15,7 @@ def bfs() :
         for dr, dc in dirs : 
             nr, nc = r + dr, c + dc
 
-            if 0<=nr<n and 0<=nc<m and a[x][y] and not visited[nr][nc] :
+            if 0<=nr<n and 0<=nc<m and a[nr][nc] and not visited[nr][nc] :
                 q.append((nr, nc))
                 visited[nr][nc] = True
 q.append((0,0))
