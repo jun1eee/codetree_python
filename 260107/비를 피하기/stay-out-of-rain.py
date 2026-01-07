8f757400629e14b759ff5bec9dec34f5d2f0fceb
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2026-01-07 22:31:02 +0900

    Title: 비를 피하기, Time: 317ms, Memory: 24MB, Status: Passed - Codetree

--- 260107/비를 피하기/stay-out-of-rain.py
+++ 260107/비를 피하기/stay-out-of-rain.py
@@ -6,16 +6,18 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 dirs = [(-1,0),(1,0),(0,-1),(0,1)]
 distance = [[0] * n for _ in range(n)]
 people = []
-place = []
+
 
 for i in range(n) :
     for j in range(n) :
         if grid[i][j] == 2 :
             people.append((i,j))
-        elif grid[i][j] == 3 :
-            place.append((i,j))
-
-def bfs() :
+        
+def bfs(sr, sc) :
+    q = deque()
+    visited = [[False] * n for _ in range(n)]
+    q.append((sr, sc, 0))
+    visited[sr][sc] = True
     while q :
         rr, cc, dist = q.popleft()
         if grid[rr][cc] == 3 :
@@ -28,13 +30,8 @@ def bfs() :
     return -1
 
 
-while people :
-    r, c = people.pop()
-    q = deque()
-    visited = [[False] * n for _ in range(n)]
-    q.append((r,c,0))
-    visited[r][c] = True
-    distance[r][c] = bfs()
+for r, c in people :
+    distance[r][c] = bfs(r,c)
 
 for i in range(n) :
     for j in range(n) :
