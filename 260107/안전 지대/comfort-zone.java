3e1a6c5b0b5ae7a642b3bf5894bf8bcaf107afce
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2026-01-07 14:45:13 +0900

    Title: 안전 지대, Time: 86ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260107/안전 지대/comfort-zone.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int n;
+    public static int m;
+    public static int[][] arr;
+    public static boolean[][] visited;
+    public static int[] dx = {-1, 0, 1, 0};
+    public static int[] dy = {0, -1, 0, 1};
+
+    public static void main(String[] args) throws IOException {
+        // k 이하인 집들은 전부 물에 잠김
+        // 안전 영역의 수가 최대가 되는 k와 그때의 안전 영역의 수 출력
+        // k가 여러개라면 그 중 가장 작은 k 출력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int maxCount = 0;
+        int maxK = 0;
+
+        int k = 1;
+
+        while (true) {    // k이상인 영역이 있을 동안
+            int count = 0;
+            visited = new boolean[n][m];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (arr[i][j] > k && !visited[i][j]) {
+                        visited[i][j] = true;
+                        dfs(i, j, k);
+                        count++;
+                    }
+                }
+            }
+
+            if (count == 0) {
+                break;
+            }
+
+            if (count > maxCount) {
+                maxCount = count;
+                maxK = k;
+            }
+            
+            k++;
+        }
+
+        System.out.print(maxK + " " + maxCount);
+    }
+
+    public static void dfs(int x, int y, int k) {
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] > k && !visited[nx][ny]) {
+                visited[nx][ny] = true;
+                dfs(nx, ny, k);
+            } 
+        }
+    }
+}
