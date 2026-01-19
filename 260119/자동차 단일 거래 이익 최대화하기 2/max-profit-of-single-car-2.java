048280311aeaaa1d268ef5cd822e29a9971f6535
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2026-01-19 21:36:39 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 3047ms, Memory: 22MB, Status: Time Limit Exceed - Codetree

--- 260119/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.java
+++ 260119/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.java
@@ -8,28 +8,62 @@ public class Main {
 
         int n = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
 
         st = new StringTokenizer(br.readLine(), " ");
 
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr1[i] = Integer.parseInt(st.nextToken());
         }
+
+        // int max = Integer.MIN_VALUE;
+        // boolean flag = false;
+
+        // for (int i = 0; i < n-1; i++) {
+        //     for (int j = i+1; j < n; j++) {
+        //         if (arr[i] > arr[j]) {
+        //             continue;
+        //         } else {
+        //             int profit = arr[j] - arr[i];
+
+        //             if (profit > max) {
+        //                 max = profit;
+        //             }
+
+        //             flag = true;
+        //         }
+        //     }
+        // }
+
+        // if (flag) {
+        //     System.out.println(max);
+        // } else {
+        //     System.out.println(0);
+        // }
+
+        for (int i = 0; i < n; i++) {
+            arr2[i] = arr1[i];
+        }
+
+        Arrays.sort(arr1);   // 2, 3, 6, 9, 10
 
         int max = Integer.MIN_VALUE;
         boolean flag = false;
 
-        for (int i = 0; i < n-1; i++) {
-            for (int j = i+1; j < n; j++) {
-                if (arr[i] > arr[j]) {
+        int profit = 0;
+
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = 0; j < n; j++) {
+                if (arr1[i] == arr2[j]) {
+                    break;
+                } else if (arr1[i] < arr2[j]) {
                     continue;
                 } else {
-                    int profit = arr[j] - arr[i];
-
+                    profit = arr1[i] - arr2[j];
                     if (profit > max) {
                         max = profit;
                     }
-
                     flag = true;
                 }
             }
