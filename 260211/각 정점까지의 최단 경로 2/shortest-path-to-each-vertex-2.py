bdd30f7e9259a4bfd2a6dda5863e27d0b1ec389f
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2026-02-11 09:38:37 +0900

    Title: 각 정점까지의 최단 경로 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260211/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
+++ 260211/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
@@ -17,4 +17,6 @@ for k in range(1, n+1):
             dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j])
 
 for i in range(1,n+1):
-    for i in range(1,n+1):
+    for j in range(1,n+1):
+        print(dist[i][j], end=" ")
+    print()
