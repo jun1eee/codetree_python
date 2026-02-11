2b15347dfbf9902596c3c29d476e13069be1377a
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2026-02-11 09:39:48 +0900

    Title: 각 정점까지의 최단 경로 2, Time: 76ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 260211/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
+++ 260211/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.py
@@ -1,22 +1,26 @@
 import sys
 input = sys.stdin.readline
-n, m = map(int, input().split())
+n = int(input())
+m = int(input())
 INF = float('inf')
 dist = [[INF]*(n+1) for _ in range(n+1)]
 
-for i in range(1, n+1):
+for i in range(1,n+1):
     dist[i][i] = 0
-
+    
 for i in range(1,m+1):
     x, y, z = map(int,input().split())
     dist[x][y] = min(dist[x][y], z)
 
-for k in range(1, n+1):
-    for i in range(1, n+1):
-        for j in range(1, n+1):
-            dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j])
+for k in range(1,n+1):
+    for i in range(1,n+1):  
+        for j in range(1,n+1):
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
 for i in range(1,n+1):
     for j in range(1,n+1):
-        print(dist[i][j], end=" ")
+        if dist[i][j] == INF:
+            print(0, end=" ")
+        else:
+            print(dist[i][j], end=" ")
     print()
