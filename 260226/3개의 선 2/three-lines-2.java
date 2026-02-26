3bc568c2a239a33f6f8ebd7b462d409cb6dda5b4
JunWonLee <162537692+jun1eee@users.noreply.github.com>
2026-02-26 17:16:23 +0900

    Title: 3개의 선 2, Time: 88ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 260226/3개의 선 2/three-lines-2.java
+++ 260226/3개의 선 2/three-lines-2.java
@@ -38,36 +38,18 @@ public class Main {
                     xSet.add(temp1X);
                 }
 
-                if (check(n, visited) && xSet.size() == 3) {
-                    System.out.println(1);
-                    return;
-                }
-            }
-        }
-
-        for (int i = 0; i < n-1; i++) {
-            int[] temp1 = pointList.get(i);
-            int temp1X = temp1[0];
-            int temp1Y = temp1[1];
-
-            for (int j = i+1; j < n; j++) {
-                int[] temp2 = pointList.get(j);
-                int temp2X = temp2[0];
-                int temp2Y = temp2[1];
-
                 if (!visited[j] && temp1Y == temp2Y) {
                     visited[i] = true;
                     visited[j] = true;
                     ySet.add(temp1Y);
                 }
 
-                if (check(n, visited) && xSet.size() + ySet.size() == 3) {
+                if (check(n, visited) && (xSet.size() == 3 || ySet.size() == 3 || xSet.size() + ySet.size() == 3)) {
                     System.out.println(1);
                     return;
                 }
             }
         }
-
     
         System.out.println(0);
     }
